ef036cce50708b5ab9f04d753e97a19c7d59885a
Shubham Gupta <shubham321@berkeley.edu>
2020-10-30 22:41:36 -0700

    Updates

---
+++ precache-manifest.8eb8731ac361e469f8e59fd60d91c55d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806e84f62ef52ca00f5a6c0b206a3e98",
+    "url": "/shivs22bday/index.html"
+  },
+  {
+    "revision": "e6ece4d9fea784784a68",
+    "url": "/shivs22bday/static/css/main.5fd97c74.chunk.css"
+  },
+  {
+    "revision": "550f4136225a735e0f6b",
+    "url": "/shivs22bday/static/js/2.31ad2dad.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/shivs22bday/static/js/2.31ad2dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ece4d9fea784784a68",
+    "url": "/shivs22bday/static/js/main.7b615ae5.chunk.js"
+  },
+  {
+    "revision": "e2627646b298d411f96d",
+    "url": "/shivs22bday/static/js/runtime-main.20fe0511.js"
+  },
+  {
+    "revision": "bdc5462e16024c35eb54bc31ad286589",
+    "url": "/shivs22bday/static/media/boys.bdc5462e.jpg"
+  },
+  {
+    "revision": "ba6b3c7a47045e24a311fcbd7d17ca2c",
+    "url": "/shivs22bday/static/media/couple.ba6b3c7a.png"
+  },
+  {
+    "revision": "e3812d36e8687bf04f2762091bf179cd",
+    "url": "/shivs22bday/static/media/donuts.e3812d36.jpg"
+  },
+  {
+    "revision": "9b4f889e4a223cb406d74e9f84e5809f",
+    "url": "/shivs22bday/static/media/equestrians.9b4f889e.jpg"
+  },
+  {
+    "revision": "134d54957639c7bb0697c9feec6119a8",
+    "url": "/shivs22bday/static/media/grad.134d5495.jpg"
+  },
+  {
+    "revision": "ffb12d1d665b82ff08f302cb04a40bd9",
+    "url": "/shivs22bday/static/media/nearme.ffb12d1d.jpg"
+  },
+  {
+    "revision": "5f833a949465bda8f62d840b2b7ba589",
+    "url": "/shivs22bday/static/media/sanramon.5f833a94.jpg"
+  },
+  {
+    "revision": "00bfd74d0e89f8981ac0d905d5a576f5",
+    "url": "/shivs22bday/static/media/superman2.00bfd74d.jpg"
+  },
+  {
+    "revision": "46ed9d37bad2a523f863a801b2c8f63d",
+    "url": "/shivs22bday/static/media/teacup.46ed9d37.jpg"
+  }
+]);
